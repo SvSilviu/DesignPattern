5d237d25f6967b69fe5037e42792b90666283183
SvSilviu <silviu.stan18@gmail.com>
2023-02-06 16:35:04 +0200

    done

---
+++ src/SingletonPattern/basic/Main.java
@@ -0,0 +1,9 @@
+package SingletonPattern.basic;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Singleton singleton = Singleton.getInstance();
+        System.out.println(singleton.getDescription());
+    }
+}
