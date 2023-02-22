d918577966abbeb1b60275df8d208c1c5661f3a2
SvSilviu <silviu.stan18@gmail.com>
2023-02-22 23:40:44 +0200

    done

---
+++ src/FacadePattern/Zoo/Elephant.java
@@ -0,0 +1,14 @@
+package FacadePattern.Zoo;
+
+public class Elephant {
+
+    public void feed(){
+        System.out.println("feedind the elephant");
+    }
+
+    public void batch(){
+        System.out.println("bathing the elephant");
+
+    }
+
+}
---
+++ src/FacadePattern/Zoo/Giraffe.java
@@ -0,0 +1,12 @@
+package FacadePattern.Zoo;
+
+public class Giraffe {
+
+    public void feed() {
+        System.out.println("Feeding the giraffe");
+    }
+
+    public void stretch() {
+        System.out.println("Helping the giraffe stretch");
+    }
+}
